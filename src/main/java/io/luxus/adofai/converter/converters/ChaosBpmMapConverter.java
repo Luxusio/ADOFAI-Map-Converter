dde726854da083753408e23c97cdee78e965d8ec
Luxusio <44326048+Luxusio@users.noreply.github.com>
2022-06-01 17:50:45 +0900

    feat: add vibration rate to chaos converter

--- src/main/java/io/luxus/adofai/converter/converters/ChaosBpmMapConverter.java
+++ src/main/java/io/luxus/adofai/converter/converters/ChaosBpmMapConverter.java
@@ -4,34 +4,49 @@ import io.luxus.adofai.converter.MapConverter;
 import io.luxus.adofai.converter.MapConverterBase;
 import io.luxus.lib.adofai.CustomLevel;
 import io.luxus.lib.adofai.LevelSetting;
+import io.luxus.lib.adofai.Tile;
 import io.luxus.lib.adofai.util.NumberUtil;
 
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class ChaosBpmMapConverter implements MapConverter {
 
     @Override
     public Object[] prepareParameters(Scanner scanner) {
-        return new Object[0];
+
+        System.out.print("진동할 각도 비율:");
+        double rate = scanner.nextDouble();
+        scanner.nextLine();
+
+        return new Object[] { rate };
     }
 
     @Override
     public boolean impossible(CustomLevel customLevel, Object... args) {
-        return false;
+        double rate = (double) args[0];
+
+        return rate >= 1.0 || rate < 0.0;
     }
 
     @Override
     public String getLevelPostfix(CustomLevel customLevel, Object... args) {
-        return "Chaos BPM";
+        return "Chaos Rate " + args[0]  + "%";
     }
 
     @Override
     public CustomLevel convert(CustomLevel customLevel, Object... args) {
+        double rate = (double) args[0];
+
         double levelBaseBpm = customLevel.getTiles().get(0).getTileMeta()
                 .getPossibleMaxBpm() * NumberUtil.randomMinMax(0.000001, 1.0);
 
         return MapConverterBase.convertBasedOnTravelAngle(customLevel, false,
-                tile -> tile.getTileMeta().getTravelAngle() == 0 ? 0 : 360.0 - NumberUtil.randomMinMax(0, 359.99999),
+                tile -> {
+                    double travelAngle = tile.getTileMeta().getTravelAngle();
+                    return travelAngle == 0 ? 0.0 :
+                            travelAngle + NumberUtil.randomMinMax(-rate, rate) * travelAngle;
+                },
                 noop -> {},
                 customLevelBuilder -> customLevelBuilder.getLevelSettingBuilder()
                         .bpm(levelBaseBpm));
